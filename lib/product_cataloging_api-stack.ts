445df38e07b8a97c06a391a668397c1078819847
yaakovepstein <yaakovesptein@gmail.com>
2021-05-10 19:15:29 -0400

    submission

--- lib/product_cataloging_api-stack.ts
+++ lib/product_cataloging_api-stack.ts
@@ -5,22 +5,35 @@ import * as lambda from '@aws-cdk/aws-lambda';
 
 
 export class ProductCatalogingApiStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // The code that defines your stack goes here
 
 
+        const table = new dynamodb.Table(this, 'Products', {
+            partitionKey: {name: 'id', type: dynamodb.AttributeType.NUMBER}
 
-    const table = new dynamodb.Table(this, 'Products', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.NUMBER }
+        });
 
-    });
 
-    const fn = new lambda.Function(this, 'InsertIntoProducts', {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      handler: 'insertIntoProducts.handler',
-      code: lambda.Code.fromAsset('lambda'),
-    });
-  }
+        const fn = new lambda.Function(this, 'InsertIntoProducts', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            handler: 'insertIntoProducts.handler',
+            code: lambda.Code.fromAsset('lambda'),
+        });
+
+
+        const retrieveFn = new lambda.Function(this, 'GetProducts', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            handler: 'getProducts.handler',
+            code: lambda.Code.fromAsset('lambda'),
+        });
+
+        const searchByTagsFn = new lambda.Function(this, 'GetProductsByTags', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            handler: 'getProductsByTags.handler',
+            code: lambda.Code.fromAsset('lambda'),
+        });
+    }
 }
